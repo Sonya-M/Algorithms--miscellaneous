8b671f202c114d1b8d818e9fcbdc88f233f4920e
Sonya-M <sonja.musicki@gmail.com>
2021-08-15 17:22:01 +0200

    added some functions

--- inJS/MyBasicFns.js
+++ inJS/MyBasicFns.js
@@ -86,6 +86,47 @@ function testRound() {
     }
 }
 
+// source: https://stackoverflow.com/questions/3177836/how-to-format-time-since-xxx-e-g-4-minutes-ago-similar-to-stack-exchange-site
+/**
+ * @param {number} timestampInMillis date in UNIX time, in milliseconds
+ * @returns {string} interval btw now and timestampInMillis, in human readable form
+ */
+export function getTimeAgo(timestampInMillis) {
+  const seconds = Math.floor((Date.now() - timestampInMillis) / 1000);
+  let interval = Math.floor(seconds / 31536000); // 31,536,000 seconds in a year
+  if (interval > 1) return `${interval} years`;
+  interval = Math.floor(seconds / 2592000); // 2,592,000 seconds in a month/30 days
+  if (interval > 1) return `${interval} months`;
+  interval = Math.floor(seconds / 86400);
+  if (interval > 1) return `${interval} days`;
+  interval = Math.floor(seconds / 3600);
+  if (interval > 1) return `${interval} hours`;
+  interval = Math.floor(seconds / 60);
+  if (interval > 1) return `${interval} minutes`;
+  return `${Math.floor(seconds)} seconds`;
+}
+
+/**
+ * @param {string} dateString string in a valid date fromat
+ * @returns date string in the format DD Mon YYYY (e.g. "25 Mar 1972")
+ */
+export function formatDate(dateString) {
+  return new Intl.DateTimeFormat('en-GB', { dateStyle: 'medium' })
+    .format(Date.parse(dateString));
+}
+
+/**
+ * @param {string} url 
+ * @returns {string} domain of given url
+ */
+export function getDomain(url) {
+  let domain = (url).substring((url).indexOf("/") + 2);
+  domain = domain.substring(0, domain.indexOf("/"));
+  if (domain.indexOf("www.") === 0) {
+    domain = domain.substring(4);
+  }
+  return domain;
+}
 
 // testRoundTo2DecPlaces();
 // testRoundToNDecPlaces();
